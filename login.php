010df93643453c0e8170059b84cf45faeee3970e
megmarieholme <35585004+megmarieholme@users.noreply.github.com>
2018-11-17 16:11:46 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -1,16 +1,16 @@
 <html>
 <body>
 <?php 
-session_start();// session for this user has been started
+session_start();
  $con = oci_connect('holme', 'Apr621997', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 if (!$con) {
 $m = oci_error();
 echo $m['message'], "\n";
-//error fuction returns an oracle message.
 exit; 
+
 $query = "SELECT first_name FROM Customer WHERE email =
  :email and password=:password"; 
-//query is sent to the db to fetch row id.
+
  $stid = oci_parse($con, $query);
 
 if (isset($_POST['email']) || isset($_POST['password']))
@@ -24,8 +24,6 @@ oci_bind_by_name($stid, ':password', $password);
 
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_ASSOC);
-
-//oci_fetch_array returns a row from the db.
 
  if ($row) {
  $_SESSION['name']=$_POST['name'];
